412230857695ce278370b1fa5f58a242eeb55ebe
Andrew Wei <andrew@variante.io>
2016-04-07 02:07:03 -0700

    Add new mixin display()

--- playground/app/scripts/main.js
+++ playground/app/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // import 'stylesheets/main.css';
-import 'stylesheets/main.scss';
-// import 'stylesheets/main.styl';
+// import 'stylesheets/main.scss';
+import 'stylesheets/main.styl';
 
 if (module.hot) module.hot.accept();
