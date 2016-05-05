5cf74ec459d15552cd262fc37799a93f69105de2
Andrew Wei <andrew@variante.io>
2016-05-05 23:01:59 +0900

    Improve original align() API

--- playground/app/scripts/main.js
+++ playground/app/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // import 'stylesheets/css/main.css';
-import 'stylesheets/sass/main.scss';
-// import 'stylesheets/stylus/main.styl';
+// import 'stylesheets/sass/main.scss';
+import 'stylesheets/stylus/main.styl';
 
 if (module.hot) module.hot.accept();
